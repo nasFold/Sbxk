f46605acd6a30f1bf7880458335a11a7c379c10a
nasFold <117340842+nasFold@users.noreply.github.com>
2024-01-28 15:05:17 +0800

    Update Score.py

--- Score.py
+++ Score.py
@@ -1,8 +1,23 @@
-# live-score-app/update_scores.py
+# termux_update.py
 import requests
 
-url = 'https://narpk.pythonanywhere.com/update_scores'
-data = {'country': 'Country 1', 'score': 3, 'time': '60:00', 'scorer': 'Player C'}
+def update_live_score(match_id, data):
+    api_url = "https://Nrlive.pythonanywhere.com/api/update_score/{}".format(match_id)
+    response = requests.post(api_url, json=data)
+    print(response.json())
 
-response = requests.post(url, data=data)
-print(response.text)
+# Contoh data yang akan diupdate
+match_id = 123
+update_data = {
+    "skor": {
+        "tim1": 2,
+        "tim2": 1
+    },
+    "waktu": "45:00",
+    "pencetak_gol": [
+        {"tim": "tim1", "pencetak": "Player A", "waktu": "30:00"},
+        {"tim": "tim2", "pencetak": "Player B", "waktu": "40:00"}
+    ]
+}
+
+update_live_score(match_id, update_data)
