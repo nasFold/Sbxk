261581fee4e86fa5d846e1d300cc97b459793ba4
nasFold <117340842+nasFold@users.noreply.github.com>
2024-01-28 12:29:56 +0800

    Update Score.py

--- Score.py
+++ Score.py
@@ -1,17 +1,8 @@
+# live-score-app/update_scores.py
 import requests
 
-def update_score(skor, waktu, pencetak_gol, negara_A, negara_B):
-    url = "http://nar.pythonanywhere.com/update-score"
-    payload = {"skor": skor, "waktu": waktu, "pencetakGol": pencetak_gol, "negaraA": negara_A, "negaraB": negara_B}
-    response = requests.post(url, json=payload)
-    return response.json()
+url = 'https://narpk.pythonanywhere.com/update_scores'
+data = {'country': 'Country 1', 'score': 3, 'time': '60:00', 'scorer': 'Player C'}
 
-# Contoh penggunaan
-skor_baru = "1-0"
-waktu_baru = "30:00"
-pencetak_gol_baru = "Player A"
-negara_A_baru = "Indonesia"
-negara_B_baru = "Belanda"
-
-result = update_score(skor_baru, waktu_baru, pencetak_gol_baru, negara_A_baru, negara_B_baru)
-print(result)
+response = requests.post(url, data=data)
+print(response.text)
